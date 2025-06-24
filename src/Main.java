cf65fd949cbe50d10ba0c0f2eb2ddb9d5b349d4e
Rikard <rikardengstrom89@gmail.com>
2025-06-24 12:33:02 +0200

    started working with Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Player player = new Player();
+        player.name = "Tim";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Player health: " + player.health);
+        player.health = 200;
+        player.loseHealth(11);
+        System.out.println("Player health: " + player.health);
+
     }
 }
