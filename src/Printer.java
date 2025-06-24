7013fa01bf6539eddb0d1b93a9df609b5fd26b1d
Rikard <rikardengstrom89@gmail.com>
2025-06-24 18:12:01 +0200

    done

--- src/Printer.java
+++ src/Printer.java
@@ -1,2 +1,36 @@
 public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        tonerLevel += tonerAmount;
+
+        if (tonerLevel > 100) {
+            return -1;
+        } else if (tonerLevel < 0) {
+            return -1;
+        } else {
+            return tonerLevel;
+        }
+    }
+
+    public int printPages(int pages) {
+
+        int pagesToPrint;
+
+        if (duplex) {
+            System.out.println("This is a duplex printer.");
+        pagesToPrint = (pages + 1) / 2;
+        } else {
+            pagesToPrint = pages;
+        } return pagesPrinted += pagesToPrint;
+    }
+
 }
---
+++ src/PrinterMain.java
@@ -0,0 +1,10 @@
+public class PrinterMain {
+
+    public static void main(String[] args) {
+
+
+        Printer printer = new Printer(65, false);
+
+        System.out.println(printer.printPages(5));
+    }
+}
