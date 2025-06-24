63bf6f6bac815a9aa3ff237883edd09ea57edf8f
Rikard <rikardengstrom89@gmail.com>
2025-06-24 17:40:24 +0200

    starting challenge

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Player player = new Player();
-        player.name = "Tim";
-        player.health = 20;
-        player.weapon = "Sword";
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Player health: " + player.health);
+//        player.health = 200;
+//        player.loseHealth(11);
+//        System.out.println("Player health: " + player.health);
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Player health: " + player.health);
-        player.health = 200;
-        player.loseHealth(11);
-        System.out.println("Player health: " + player.health);
+        EnchancedPlayer rikard = new EnchancedPlayer("Rikard", 99, "Axe");
+        System.out.println("Initial health is " + rikard.healthRemaining());
 
     }
 }
